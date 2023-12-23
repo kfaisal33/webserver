95ad1411d9b7d47e14f6c24867f7bd46bb47be5b
kfaisal33 <kfaisal33@gmail.com>
2023-12-24 02:03:32 +0530

    Web Server Project

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
-import main
+package main
 
-import(
+import (
 	"fmt"
+	"log"
+	"net/http"
 )
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST Request Successful")
+	name := r.FormValue("fname")
+	last := r.FormValue("lname")
+	fmt.Fprintf(w, "Name =%s\n", name)
+	fmt.Fprintf(w, "Name =%s\n", last)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprint(w, "Hello!")
+}
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server Is Running On Port 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
